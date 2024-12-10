a4adf927f3d9674b06aa1d82cb4d45016c981faf
apni-coding <apnicoding72@gmail.com>
2024-12-10 08:47:44 +0530

    simple express server create

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// root file (sever created)
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// server listen
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  try {
+    console.log("App is running on 30001");
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
