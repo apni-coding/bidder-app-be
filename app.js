e72fd08411eb54e7ea1fefc50360da884d1770e6
Vivek Kumar <123900354+apni-coding@users.noreply.github.com>
2024-12-14 20:12:30 +0530

    Feature/role user model (#7)
    * roles model create
    
    * user model create

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const sequelize = require("./src/config/dbConnect");
+const Roles = require("./src/models/role");
+const Users = require("./src/models/user");
 const app = express();
 
 app.use(express.json());
@@ -11,11 +13,14 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, async () => {
   try {
-    console.log("App is running on 30001");
+    console.log(`App is running on ${PORT}`);
     // db connection
     await sequelize.authenticate();
     console.log('Db Connection has been established successfully.');
+    await Roles.sync({ force: false });
+    await Users.sync({force: false})
   } catch (error) {
     console.log("Error", error.message);
   }
 });
+
