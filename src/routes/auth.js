d2356920bb4b627999493244f05373a9b7d6c1ce
apni-coding <apnicoding72@gmail.com>
2024-12-18 09:43:44 +0530

    add validation for login api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 const authController = require("../controoler/auth/index");
 const validateSchema = require("../middlewares/validator");
 const registerSchema = require("../middlewares/validationSchema/resigsterUser");
+const loginSchema = require("../middlewares/validationSchema/loginUser");
 const authRouter = express.Router();
 
 authRouter.post("/register", validateSchema(registerSchema), authController.registerUser);
 authRouter.get("/verify-account/:token", authController.verifyAccount);
-authRouter.post("/login", authController.loginUser);
+authRouter.post("/login", validateSchema(loginSchema), authController.loginUser);
 
 module.exports = authRouter;
 
