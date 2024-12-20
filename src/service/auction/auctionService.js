64fd26cc73ec51dd490c6527c99d4e48da4e7f19
apni-coding <apnicoding72@gmail.com>
2024-12-20 11:12:24 +0530

    update auction api

--- src/service/auction/auctionService.js
+++ src/service/auction/auctionService.js
@@ -1,4 +1,5 @@
 const Auction = require("../../models/auction");
+const { ERROR_MESSAGE } = require("../../utils/propertyResolver");
 
 const createAuction = async (auctionData, userId) => {
   try {
@@ -12,6 +13,35 @@ const createAuction = async (auctionData, userId) => {
   }
 };
 
+const updateAuction = async (auctionId, userId, auctionData) => {
+  try {
+    // Find the auction by ID and ensure the user is the creator
+    const auction = await Auction.findOne({
+      where: {
+        id: auctionId,
+        created_by: userId,
+      },
+    });
+
+    // Auction not found
+    if (!auction) {
+      throw new Error(ERROR_MESSAGE.AUCTION_NOT_FOUND);
+    }
+
+    // Update auction filed and reset status to 'pending'
+    await auction.update({
+      ...auctionData,
+      status: "pending",
+      updated_by: userId,
+    });
+
+    return auction;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
 module.exports = {
   createAuction,
+  updateAuction,
 };
